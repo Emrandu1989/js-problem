f8dd36d3d3a0cbb63d375aebadfa27c63f4acc1b
Emrandu1989 <emrandu1989@gmail.com>
2024-02-03 05:25:29 +0600

    added task1 in the js-string-task file

--- js-string-task/task1.js
+++ js-string-task/task1.js
@@ -1,2 +1,28 @@
 /* Task-1:
 Count how many times a string has the letter a */
+
+
+
+let string = 'imran is a good boy and you should like him';
+// let count = 0;
+// for(let i = 0; i< string.length; i++){
+//     if(string[i] === "a" ){
+//         count++;
+//     }
+// }
+// console.log(count)
+
+function countLetterA(str){
+    let count = 0;
+    for( let i = 0; i< str.length; i++){
+        if(str[i].toLowerCase() === "a"){
+            count++
+        }
+    }
+    return count;
+
+}
+
+let myString = "An apple a day keeps the doctor away";
+const result = countLetterA(myString);
+console.log(result);
